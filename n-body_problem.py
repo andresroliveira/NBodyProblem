0703face9c9416f3a725dce76fed2a3c7856ef61
Andrês <andresroliveira98@gmail.com>
2022-07-15 15:48:38 -0300

    Update n-body_problem.py

--- n-body_problem.py
+++ n-body_problem.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pandas as pd
 from scipy.integrate import solve_ivp
@@ -7,12 +8,16 @@ import matplotlib.colors as mcolors
 from time import time
 
 G = 1
+EPS = 1e-6
 colors = list(mcolors.TABLEAU_COLORS)
 
 
 def force(pos1, pos2, m1, m2):
     r = pos2 - pos1
-    f = (G * m1 * m2 / (np.linalg.norm(r)**3)) * r
+    l = np.linalg.norm(r)
+    if np.abs(l) < EPS:
+        raise Exception('divide by 0')
+    f = (G * m1 * m2 / (l**3)) * r
     return f
 
 
@@ -44,7 +49,12 @@ def state_to_pos(S, n):
 
 def read_data(file: str):
     print('read data')
-    data = pd.read_csv(file)
+    try:
+        data = pd.read_csv(file)
+    except FileNotFoundError:
+        print("File not Found")
+        input("Press Enter to continue...")
+        exit(0)
     n = data.shape[0]
     S0 = np.concatenate([data["x"], data["y"], data["vx"], data["vy"]],
                         dtype="float")
@@ -53,28 +63,49 @@ def read_data(file: str):
     return n, S0, m
 
 
-def main():
-    # file = input('enter a csv file: ')
-    file = 'pvi_4'
-    # tmax = float(input('enter a tmax: '))
-    tmax = 10
+def save_sol(sol, t, n):
+    dic = {}
+    dic['t'] = t
+    for i in range(n):
+        dic['x_' + str(i + 1)] = sol[:, i]
+        dic['y_' + str(i + 1)] = sol[:, i + n]
+        dic['vx_' + str(i + 1)] = sol[:, i + 2 * n]
+        dic['vy_' + str(i + 1)] = sol[:, i + 3 * n]
+
+    df = pd.DataFrame(dic)
+    return df
+
+
+def main(file: str, tmax: float, fps: float):
+    """
+        Parameters: 
+        
+        file: string
+        file.csv should be a csv file with x,y,vx,vy,m collums
+
+        tmax: float
+        solve ivp problem in the interval [0, tmax] with initial conditions in file.csv. Default is 10.0
+
+        fps: float
+        frames per second of resulting imagem. Default is 26.0
+    """
     n, S0, m = read_data(file + ".csv")
     pos = state_to_pos(S0, n)
-    dt = 1. / 1000
-    t_eval = np.arange(0, tmax, dt)
+    t_eval = np.linspace(0, tmax, int(tmax * fps * 10))
     print('prepare to solve')
-    sol_ivp = solve_ivp(gravitation,
-                        t_span=[0, tmax],
-                        y0=S0,
-                        args=(n, m),
-                        method='DOP853',
-                        t_eval=t_eval,
-                        rtol=1e-10,
-                        atol=1e-15)
+    sol_ivp = solve_ivp(
+        gravitation,
+        t_span=[0, tmax],
+        y0=S0,
+        args=(n, m),
+        method='Radau',
+        # t_eval=t_eval,
+        # rtol=1e-10,
+        # atol=1e-15
+    )
 
     if not sol_ivp.success:
         raise Exception("Not success solve_ivp!")
-        raise Exception(sol_ivp.message)
         exit(0)
 
     print('solved')
@@ -82,9 +113,20 @@ def main():
     t = sol_ivp.t
     dt = t[1] - t[0]
 
+    # print(sol)
+    # print(sol_ivp)
+
+    df = save_sol(sol, t, n)
+    df.to_csv('outputs/sol_' + file + '.csv', index=False)
+    return
+
     print(sol.shape)
+    print(t.shape)
     print(state_to_pos(sol[0], n))
-    print('fps =', 1 / dt)
+    # print('fps =', fps)
+
+    # for i in range(1, len(t)):
+    #     print(t[i] - t[i-1])
 
     print('animate')
     fig = plt.figure(figsize=(5, 5))
@@ -98,20 +140,21 @@ def main():
 
     def update(i):
         sca.set_offsets(state_to_pos(sol[i], n))
-        ttx.set_text('time = %.2f' % t[i])
+        ttx.set_text('time = %.3f' % t[i])
         return sca, ttx
 
     t0 = time()
     update(0)
     t1 = time()
 
-    anim = FuncAnimation(fig,
-                         update,
-                         frames=len(t),
-                         interval=1000 * dt - (t1 - t0),
-                         init_func=init)
+    anim = FuncAnimation(
+        fig,
+        update,
+        frames=len(t),
+        #  interval=1000 * (1 / (tmax * 10 * fps)) - (t1 - t0),
+        init_func=init)
 
-    anim.save('sol_' + file + '.gif', fps=1 / dt)
+    anim.save('outputs/sol_' + file + '.gif')
     plt.show()
 
     print('done')
@@ -119,4 +162,25 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        try:
+            file = sys.argv[1]
+        except:
+            print('no file select')
+            exit(0)
+
+        try:
+            tmax = float(sys.argv[2])
+        except:
+            tmax = 10.0
+
+        try:
+            fps = float(sys.argv[3])
+        except:
+            fps = 26.0
+    else:
+        file = 'ivp'
+        tmax = 10.0
+        fps = 26.0
+
+    main(file, tmax, fps)
